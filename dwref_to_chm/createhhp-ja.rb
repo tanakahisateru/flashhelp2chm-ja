4b9b38a20cb75062a15827e723f55e7b5e21ccfb
tanakahisateru <tanakahisateru@59c56cfd-2740-0410-bd3f-6725098f0ba8>
2007-12-05 17:48:30 +0000

    dwref_to_chm supports keyword index.
    git-svn-id: https://flashhelp2chm-ja.googlecode.com/svn/trunk@14 59c56cfd-2740-0410-bd3f-6725098f0ba8

--- dwref_to_chm/createhhp-ja.rb
+++ dwref_to_chm/createhhp-ja.rb
@@ -1,5 +1,6 @@
 require "rexml/document"
 require "kconv.rb"
+require "CGI"
 include REXML
 
 def header()
@@ -10,16 +11,66 @@ def footer()
 	return '</BODY></HTML>'
 end
 
-def formatTopicItem(name, href, bookpath)
+def formatTopicItem(name, href)
 	out = '<LI><OBJECT type="text/sitemap">'
 	out += sprintf('<param name="Name" value="%s">', name)
 	if href != nil then
-		out += sprintf('<param name="Local" value="%s/%s">', bookpath, href)
+		out += sprintf('<param name="Local" value="%s">', href)
 	end
 	out += "</OBJECT>"
 	return out
 end
 
+def formatIndexItem(name, details)
+	out = ""
+	if details.length <= 1 then
+		out += '<LI><OBJECT type="text/sitemap">'
+		out += sprintf('<param name="Name" value="%s">', name)
+		out += sprintf('<param name="Local" value="%s">', details[0]['href'])
+		out += "</OBJECT>\n"
+	else
+		minelem = 100
+		details.each() do |det|
+			if minelem > det['placement'].size then
+				minelem = det['placement'].size
+			end
+		end
+		
+		path_depth_to_remove = 0
+		for col in 0...minelem
+			has_diff = false
+			prev_one = nil
+			details.each() do |det|
+				path = det['placement']
+				if prev_one == nil then
+					prev_one = path[col]
+				elsif path[col] != prev_one then
+					has_diff = true
+				end
+			end
+			if has_diff then
+				break
+			end
+			path_depth_to_remove += 1
+		end
+		
+		details.each() do |det|
+			out += '<LI><OBJECT type="text/sitemap">'
+			if path_depth_to_remove == minelem then
+				out += sprintf('<param name="Name" value="%s">', name)
+			else
+				out += sprintf('<param name="Name" value="%s (%s)">', name, det['placement'][path_depth_to_remove..-1].join(' '))
+			end
+			out += sprintf('<param name="Local" value="%s">', det['href'])
+			out += "</OBJECT>\n"
+		end
+		
+		#if path_depth_to_remove >= 1 then
+		#	print sprintf("about %s : merge:%s(depth %d/%d)\n", name, details[0]['placement'][0...path_depth_to_remove].join(' '), path_depth_to_remove, minelem)
+		#end
+	end
+	return out
+end
 
 def firstpageOfBook(book)
 	book.elements.each() do |topic|
@@ -28,16 +79,19 @@ def firstpageOfBook(book)
 	end
 end
 
-def parseBooks(bookpath, file)
+def parseBook(bookpath, file, keyword_index)
 	
 	print bookpath + "/Reference.xml "
 	
 	book = Document.new(File.new(bookpath + "/Reference.xml")).root
+	title = CGI.escapeHTML(book.attributes['name'].tosjis)
+	print title +"\n"
 	
-	print  book.attributes['name'].tosjis + "\n"
+	idx_title = title.split(' ')
+	idx_title.shift() if idx_title[0] == "O'REILLY"
+	idx_title.pop() if idx_title[-1] == "リファレンス"
 	
-	title = book.attributes['name'].tosjis
-	file.puts "\t" + formatTopicItem(title, "default.html", bookpath)
+	file.puts "\t" + formatTopicItem(title, bookpath + "/default.html")
 	
 	file.puts "\t" + "<UL>"
 	
@@ -51,34 +105,65 @@ def parseBooks(bookpath, file)
 	
 	clv = 0
 	topics.each() do |topic|
-		file.puts "\t" * 2 + formatTopicItem(
-			topic.attributes['name'].tosjis,
-			topic.attributes['location'],
-			bookpath
-		)
+		name = CGI.escapeHTML(topic.attributes['name'].tosjis)
+		href = bookpath + "/" + topic.attributes['location']
+		file.puts "\t" * 2 + formatTopicItem(name, href)
 		file.puts "\t" * 2 + '<UL>'
 		
+		appendKeywordIfUnique(keyword_index, name, [idx_title, name], href)
+		
 		topic.elements.each("subtopic") do |subtopic|
-			file.puts "\t" * 3 + formatTopicItem(
-				subtopic.attributes['name'].tosjis,
-				topic.attributes['location'] + "#" + subtopic.attributes['id'].tosjis,
-				bookpath
-			)
+			sub_name = CGI.escapeHTML(subtopic.attributes['name'].tosjis)
+			sub_href = href + "#" + subtopic.attributes['id'].tosjis
+			file.puts "\t" * 3 + formatTopicItem(sub_name, sub_href)
+			appendKeywordIfUnique(keyword_index, sub_name, [idx_title, name, sub_name], sub_href)
 		end
+		
 		file.puts "\t" * 2 + '</UL>'
 	end
 	
 	file.puts "\t" + '</UL>'
 end
 
-def createTocFile(filename, books)
+def appendKeywordIfUnique(keyword_index, name, docpath, href)
+	if keyword_index[name] == nil then
+		keyword_index[name] = []
+	end
+	is_newpage = true
+	keyword_index[name].each() do |det|
+		if det['href'] == href then
+			is_newpage = false
+			break
+		end
+	end
+	if is_newpage then
+		keyword_index[name].push({'placement'=>docpath[0...-1], 'href'=>href})
+	end
+end
+
+def createContentsFiles(toc_filename, idx_filename, books)
 	
-	file = open(filename, 'w')
+	keyword_index = {}
+	
+	file = open(toc_filename, 'w')
 	file.puts header()
 	
 	file.puts "<UL>"
 	books.each() do |bookpath|
-		parseBooks(bookpath, file)
+		parseBook(bookpath, file, keyword_index)
+	end
+	file.puts "</UL>"
+	
+	file.puts footer()
+	file.close()
+	
+	#Keywords
+	file = open(idx_filename, "w")
+	file.puts header()
+	
+	file.puts "<UL>"
+	keyword_index.keys.sort().each() do |name|
+		file.puts(formatIndexItem(name, keyword_index[name]))
 	end
 	file.puts "</UL>"
 	
@@ -99,7 +184,7 @@ def collectAssets(assetsdir, assetsexplise)
 	return assets
 end
 
-def createProjectFile(filename, title, toc, target, toppage, assetsdir, assetsexplise)
+def createProjectFile(filename, title, toc, idx, target, toppage, assetsdir, assetsexplise)
 	file = open(filename, 'w')
 	file.puts('[OPTIONS]')
 	file.puts('Compatibility=1.1 or later')
@@ -108,6 +193,7 @@ def createProjectFile(filename, title, toc, target, toppage, assetsdir, assetsex
 	file.puts('Display compile progress=Yes')
 	file.puts('Default topic=' + toppage) unless toppage == nil
 	file.puts('Full-text search=Yes')
+	file.puts('Index file=' + idx)
 	file.puts('Language=0x411 日本語')
 	file.puts('Title=' + title)
 	file.puts('')
@@ -131,15 +217,16 @@ prjname = ARGV[0]
 basedir = ARGV[1]
 title   = ARGV[2]
 
-createTocFile(prjname+'.hhc', [
-		basedir + "/HTML",
-		basedir + "/CSS",
-		basedir + "/JavaScript"
+createContentsFiles(prjname+'.hhc', prjname+'.hhk', [
+	basedir + "/HTML",
+	basedir + "/CSS",
+	basedir + "/JavaScript"
 ])
 	
 createProjectFile(
 	prjname+'.hhp', title,
-	prjname+'.hhc', prjname+'.chm',
+	prjname+'.hhc', prjname+'.hhk',
+	prjname+'.chm',
 	'index.html',
 	basedir, /^(.*\.xml|.*(lookupMod|ASP|CF|JIS|JSP|PHP|SQL|Usable|XML|XSLT).*)$/
 )
